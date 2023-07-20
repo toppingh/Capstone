ec486d87a02f5981db782c33dd408807952db376
topping <reyna11@naver.com>
2023-07-21 01:09:21 +0900

    0721 mysettings.py 추가

---
+++ project/mysettings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CapstoneProject_db',
+        'USER': 'topping',
+        'PASSWORD': 'ekdms1122',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
