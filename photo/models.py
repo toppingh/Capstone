84ce0f8ceb346fd75dbc85c7edd3ed96c934427b
topping <reyna11@naver.com>
2023-07-23 07:26:27 +0900

    photo_state 수정

--- photo/models.py
+++ photo/models.py
@@ -4,7 +4,7 @@ from accounts.models import User
 # Create your models here.
 # 사진
 class Photo(models.Model):
-    email = models.EmailField(blank=False) # 이메일은 필수, 유저 구분용
+    email = models.EmailField() # 이메일은 필수, 유저 구분용
     image = models.ImageField(upload_to='photos/')
     photo_state = (
         ('GOOD', 'normal'),
@@ -12,9 +12,9 @@ class Photo(models.Model):
         ('ETC', 'etc'),
     )
     name = models.CharField(max_length=60)
-    ai_name = models.CharField(max_length=60)
-    state = models.CharField(max_length=20, choices=photo_state, default='GOOD')
-    explain = models.CharField(max_length=60) # 이미지에 대한 결과 코멘트
+    ai_name = models.CharField(max_length=60, null=True, blank=True)
+    state = models.CharField(max_length=20, choices=photo_state, default='GOOD', null=True, blank=True)
+    explain = models.CharField(max_length=60, null=True, blank=True) # 이미지에 대한 결과 코멘트
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
