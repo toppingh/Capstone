c0a7988dc9df2e0f551e57f212f26794d6ebceaf
topping <reyna11@naver.com>
2023-07-23 02:06:03 +0900

    0723 accounts 모델 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,57 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
-from .manager import UserManager
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not username :
+            raise ValueError("아이디를 입력해야 합니다.")
+        if not email :
+            raise ValueError("이메일을 입력해야 합니다.")
 
-class User(AbstractUser):
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            password=password
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=30, unique=True)
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    profileImg = models.ImageField(upload_to='profiles/%Y/%m/%d', default='default.png', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'
+    date_joined = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'username' # username, 아이디로 사용할 필드
     REQUIRED_FIELDS = ['email']
 
     objects = UserManager()
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        ordering = ['date_joined']
