72c79f564648f2abf41c352e5521e39802e6027c
topping <reyna11@naver.com>
2023-07-23 06:31:04 +0900

    photo 앱 모델 수정

--- photo/models.py
+++ photo/models.py
@@ -1,22 +1,19 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 # 사진
 class Photo(models.Model):
-    resultCode = models.CharField(max_length=1)
-    date = models.DateTimeField()
+    email = models.ForeignKey(User.email, blank=False) # 이메일은 필수, 유저 구분용
+    image = models.ImageField(upload_to='photos/')
+    state = (
+        ('0', '정상'),
+        ('B', '토마토 잎 곰팡이병'),
+        ('C', '토마토 황화 잎말이 바이러스'),
+    )
+    name = models.CharField(max_length=1, choices=state)
+    explain = models.CharField(max_length=100) # 이미지에 대한 결과 코멘트
+    create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
-
-# 정상
-class Normal(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True, related_name='normal_photos')
-    content = models.TextField(blank=True)
-    img = models.ImageField(upload_to='normal_photos/')
-
-# 병충해
-class Infect(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.SET_NULL, null=True, related_name='infect_photos')
-    content = models.TextField(blank=True)
-    img = models.ImageField(upload_to='infect_photos/')
+        return self.name
