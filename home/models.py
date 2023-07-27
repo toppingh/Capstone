0c42f44896ee63cca03d2bba81f41540e37c0628
topping <reyna11@naver.com>
2023-07-28 04:10:38 +0900

    0728 home/models.py blight 타입 추가

--- home/models.py
+++ home/models.py
@@ -4,11 +4,16 @@ from django.db import models
 # 자주 발병하는 병해
 from django.utils import timezone
 
-
 class Blight(models.Model):
     name = models.CharField(max_length=20)
     blight_img = models.ImageField(upload_to='blights/%Y/%m/%d', default='default.png')
     causation = models.TextField()
+    blight_type = (
+        ('DOT', 'CMV'), # 반점
+        ('SLIM', 'TMV'), # 얇아짐
+        ('STRIPE', 'ToMV'), # 줄무늬
+
+    )
 
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
@@ -38,3 +43,12 @@ class Pest(models.Model):
 class History(models.Model):
     name = models.CharField(max_length=20)
     history_img = models.ImageField(upload_to='histories/%Y/%m/%d', default='default.png')
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
