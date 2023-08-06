6fb8908b0f506d67243a6d3e37c2cc2aa16d88e2
topping <reyna11@naver.com>
2023-08-06 14:28:13 +0900

    0806 flask api 응답 코드 추가

--- photo/views.py
+++ photo/views.py
@@ -2,7 +2,7 @@ import os
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import requests
 from PIL import Image
 
@@ -88,36 +88,16 @@ def send_photo_to_AI(request):
 
             # 요청 결과 처리
             if response.status_code == 200:
-                return JsonResponse({'message': "사진을 성공적으로 보냈습니다!"})
+                with open('default_image.jpg', 'wb') as f: # flask에서 보낸 이미지 받기
+                    f.write(response.content)
+
+                with open('default_image.jpg', 'wb') as f:
+                    response = HttpResponse(f.read(), content_type='image/jpg')
+                    response['Content-Disposition'] = '"attachment; filename="default_image.jpg"'
+                    return response
             else:
-                return JsonResponse({'message': "사진을 보내는데 실패했습니다! 다시 시도해주세요."}, status=400)
+                return HttpResponse({'message': "사진을 보내는데 실패했습니다! 다시 시도해주세요."}, status=response.status_code)
         else:
-            return JsonResponse({'message': "사진 파일을 찾을 수 없습니다."}, status=404)
+            return HttpResponse({'message': "사진 파일을 찾을 수 없습니다."}, status=404)
 
-    return JsonResponse({"message": "잘못된 요청입니다!"}, status=405)
-
-# # flask api와 통신
-# def detect_objects(request):
-#     flask_api_url = 'http://127.0.0.1:5000/detect' # flask api url
-#     #django_api_url = 'http://127.0.0.1:8000/photo/all'
-#
-#     #root = Tk()
-#     #root.withdraw()
-#     # 이미지 선택 대화상자 부르기
-#     image_files_path = filedialog.askopenfilename(title='Images', filetype=[('Image Files', ['.jpg', '.png', '.jpeg', '.gif'])])
-#     #root.destroy()
-#
-#     if not image_files_path:
-#         print('이미지가 선택되지 않았습니다.')
-#         return
-#
-#     # flask api에 요청 보내기
-#     files = {'image':image_files_path}
-#     response = requests.post(flask_api_url, files=files) # requests로 flask rest api로 전달
-#
-#     # flask api의 응답을 JSON으로 반환
-#     if response.status_code == 200:
-#         result = response.json()
-#         return JsonResponse(result, status=200)
-#     else:
-#         return JsonResponse({'error': 'Flask API와 연결 실패'}, status=500)
+    return HttpResponse({"message": "잘못된 요청입니다!"}, status=405)
